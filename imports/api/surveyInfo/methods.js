c047ba389278ed4758ab36b1c28952edbbeb8055
lushomo chimimba <lushomochimimba9@gmail.com>
2019-03-11 15:02:16 +0200

    added comments to the code

--- imports/api/surveyInfo/methods.js
+++ imports/api/surveyInfo/methods.js
@@ -2,15 +2,12 @@ import { Meteor } from  'meteor/meteor';
 import surveyData from './collection.js'
 
 Meteor.methods({
-        
+ // inserting data collected from answers given to survey questions into surveyData collection
     'SData.create':(Data)=>{
-
-        console.log("yabwela: ",Data);
         
-
+    console.log("data arrived",Data);
     const result = surveyData.insert(Data);
 
     return result;
     }
-
 })
--- imports/ui/surveyjs.jsx
+++ imports/ui/surveyjs.jsx
@@ -4,17 +4,8 @@ import {withTracker} from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import surveyData from "../api/surveyInfo/collection.js"
 
+// json object with questions and their choices(options).
 var surveyJSON = {pages:[{name:"page1",elements:[{type:"radiogroup",name:"question2",title:"who are we ",choices:[{value:"item1",text:"Humans "},{value:"item2",text:"Aliens"},{value:"item3",text:"neither"}]},{type:"radiogroup",name:"question1",title:"what do we live for",choices:[{value:"item1",text:"to see the next day"},{value:"item2",text:"vanity"},{value:"item3",text:"food"}]},{type:"radiogroup",name:"question3",title:"where should you get your gas from",choices:[{value:"item1",text:"pum"},{value:"item2",text:"engi"},{value:"item3",text:"TOTAL"}]},{type:"checkbox",name:"question4",title:"how can i",choices:[{value:"item1",text:"live "},{value:"item2",text:"with"},{value:"item3",text:"without"},{value:"item4",text:"out"}]}],visible:true}]}
-
-
-
-function sendDataToServer(survey) {
-  survey.sendResult('e544a02f-7fff-4ffb-b62d-6a9aa16efd7c');
-  // console.log(resultAsString)
-  var resultAsString = JSON.stringify(survey.data);
-  // alert(resultAsString)
-  alert(resultAsString)
-}
 
 export class Mysurveys extends Component {
   state = {
@@ -22,23 +13,19 @@ export class Mysurveys extends Component {
   }
   
 getSurveyData=(survey)=>{
+  // assigning the variable Data to the data collected from the responses given from the survey question (data is returned in an object)
+    var Data =  JSON.stringify(survey.data);
 
-
-// var Data = {
-//       userData: JSON.stringify(survey.data)
-// }
-var Data =  JSON.stringify(survey.data)
-
-  // var Data = {name:{}}
-  Meteor.call('SData.create',Data,(err,result)=>{result ? console.log('inserted data successfully'): console.log('failed to insert data')});
-  console.log(JSON.stringify(survey.data))
+// checking if object is empty before inserting into collection.
+    (Object.getOwnPropertyNames(survey.data).length === 0 ? console.log("please answer at least one questions before submitting") :  Meteor.call('SData.create',Data,(err,result)=>{result ? console.log('inserted data successfully'): console.log('failed to insert data')}))
+    // console the data collected from answers given to survey questions
+    console.log(Data)
 }
 
   render() {
     return (
       <div>
       <div id="surveyContainer">
-      <h1>hello</h1>
       <Survey.Survey json={ surveyJSON } onComplete={this.getSurveyData} />
       </div>
       </div>
@@ -51,7 +38,7 @@ export default withTracker(() => {
 
 
   return {
-    // links: Links.find().fetch(),
+    
     SData : surveyData.find().fetch()
   };
 })(Mysurveys);
